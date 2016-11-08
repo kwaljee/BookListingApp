fa68653faa4731d9b6c47ddcfba31fb7b7a2c247
kwaljee <kwaljee@gmail.com>
2016-11-08 11:56:01 -0500

    Removed unused imports

--- app/src/main/java/com/example/android/booklistingapp/MainActivity.java
+++ app/src/main/java/com/example/android/booklistingapp/MainActivity.java
@@ -3,26 +3,11 @@ package com.example.android.booklistingapp;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.TextUtils;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.nio.charset.Charset;
-
-import static android.R.attr.author;
 
 
 public class MainActivity extends AppCompatActivity {
