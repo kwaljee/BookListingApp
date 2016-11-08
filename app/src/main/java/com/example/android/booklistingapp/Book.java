a4644dda044f85ac00b56c249eca5c7fcf47f90c
kwaljee <kwaljee@gmail.com>
2016-11-08 11:54:53 -0500

    Initial commit

---
+++ app/src/main/java/com/example/android/booklistingapp/Book.java
@@ -0,0 +1,15 @@
+package com.example.android.booklistingapp;
+
+
+public class Book {
+
+    public final String title;
+    public final String authors;
+
+
+    public Book(String bookTitle, String bookAuthors) {
+        title = bookTitle;
+        authors = bookAuthors;
+    }
+}
+
---
+++ app/src/main/java/com/example/android/booklistingapp/MainActivity.java
@@ -0,0 +1,96 @@
+package com.example.android.booklistingapp;
+
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.nio.charset.Charset;
+
+import static android.R.attr.author;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+    public static final String LOG_TAG = MainActivity.class.getSimpleName();
+
+    private static String urlString;
+
+    private static final String TAG_TITLE = "title";
+    private static final String TAG_AUTHORS = "authors";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btn = (Button) findViewById(R.id.search_btn);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText input = (EditText) findViewById(R.id.search_txt);
+                String query = input.getText().toString().replace(" ", "+");
+                urlString = "https://www.googleapis.com/books/v1/volumes?q=" + query + "&orderBy=newest";
+                new ParseJSON().execute(urlString);
+            }
+        });
+    }
+
+
+    private class ParseJSON extends AsyncTask<String, Void, Book> {
+        @Override
+        protected Book doInBackground(String... urls) {
+
+            String url = urlString;
+            return null;
+        }
+
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            Toast.makeText(MainActivity.this, "Loading results, please wait...", Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        protected void onPostExecute(Book book) {
+            if (book == null) {
+                return;
+            }
+
+            updateUI(book);
+        }
+    }
+
+    private void updateUI(Book book) {
+
+        TextView titleTextView = (TextView) findViewById(R.id.book_title);
+        titleTextView.setText(book.title);
+
+        TextView authorsTextView = (TextView) findViewById(R.id.book_authors);
+        authorsTextView.setText(book.authors);
+
+    }
+
+
+}//End of MainActivity
+
+
+
